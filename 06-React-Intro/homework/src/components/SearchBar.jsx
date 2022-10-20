3cfc5f53dc1629ffa346708a257c55f18ebd8410
Salvador Hilares <salvador.hilares@utec.edu.pe>
2022-10-20 10:10:44 -0500

    First react

---
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SearchBar({onSearch}) {
+  // acá va tu código
+  return <div>
+    <input type="text" placeholder='Ciudad...'/>
+    <button onClick={()=>onSearch('Valor del input')}>Buscar</button>
+  </div>
+};
